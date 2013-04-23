9ffd26ce741e0492ca51ce2a1d2a71b3337ba973
Philip Brown <phil@ipbrown.com>
2013-04-23 19:59:07 +0100

    Derp, add database name

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
+			'database'  => 'cribbb',
 			'username'  => 'cribbb',
 			'password'  => '',
 			'charset'   => 'utf8',
