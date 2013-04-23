5a5540bf81005e38af0407d0252fc95a045491db
Philip Brown <phil@ipbrown.com>
2013-04-23 19:15:36 +0100

    Add database connection

--- app/config/database.php
+++ app/config/database.php
@@ -56,7 +56,7 @@ return array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
 			'database'  => 'database',
-			'username'  => 'root',
+			'username'  => 'cribbb',
 			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
