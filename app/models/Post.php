931579b56b6b311b5f94e1fcdc883a231f30ab46
Philip Brown <phil@ipbrown.com>
2013-04-25 21:08:16 +0100

    Add validation for user_id on Post Model

--- app/models/Post.php
+++ app/models/Post.php
@@ -6,7 +6,8 @@ class Post extends Ardent {
     protected $guarded = array('id', 'created_at', 'updated_at');
 
     public static $rules = array(
-      'body' => 'required',
+      'body'    => 'required',
+      'user_id' => 'required|numeric'
     );
 
     public function user()
