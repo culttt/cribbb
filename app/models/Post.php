e3b7951c9d2124d6e5f2b9b316fc2b338182bfac
Philip Brown <phil@ipbrown.com>
2013-04-25 19:49:54 +0100

    Add Post relationship to User

--- app/models/Post.php
+++ app/models/Post.php
@@ -1,7 +1,15 @@
 <?php
 
 class Post extends Eloquent {
-    protected $guarded = array();
 
-    public static $rules = array();
+    protected $guarded = array('id', 'created_at', 'updated_at');
+
+    public static $rules = array(
+      'body' => 'required',
+    );
+
+    public function user()
+    {
+      return $this->belongs_to('User');
+    }
 }
