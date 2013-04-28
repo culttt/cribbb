2030b09699dc2697c7e26fc7e9c80e2435833ee2
Philip Brown <phil@ipbrown.com>
2013-04-28 19:07:08 +0100

    Add users table and migrations

---
+++ app/database/migrations/2013_04_28_180505_add_username_to_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddUsernameToUsersTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::table('users', function(Blueprint $table) {
+        $table->string('username');
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      Schema::table('users', function(Blueprint $table) {
+        $table->dropColumn('username');
+      });
+    }
+
+}
