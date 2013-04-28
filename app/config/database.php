3a7213c055b9cb24506f2bdb98c419ff992c8bd8
Philip Brown <phil@ipbrown.com>
2013-04-28 18:34:46 +0100

    Add database config

--- app/config/database.php
+++ app/config/database.php
@@ -55,8 +55,8 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
-			'username'  => 'root',
+			'database'  => 'cribbb',
+			'username'  => 'cribbb',
 			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
