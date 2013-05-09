3a96f9871a352fe1615af6c302b975c376589704
Philip Brown <phil@ipbrown.com>
2013-05-09 19:11:14 +0100

    Update User Model

--- app/models/User.php
+++ app/models/User.php
@@ -2,51 +2,67 @@
 
 use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableInterface;
+use LaravelBook\Ardent\Ardent;
 
-class User extends Eloquent implements UserInterface, RemindableInterface {
+class User extends Ardent implements UserInterface, RemindableInterface {
 
-	/**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
-	protected $table = 'users';
+  /**
+   * The database table used by the model.
+   *
+   * @var string
+   */
+  protected $table = 'users';
 
-	/**
-	 * The attributes excluded from the model's JSON form.
-	 *
-	 * @var array
-	 */
-	protected $hidden = array('password');
+  /**
+   * Post relationship
+   */
+  public function posts()
+  {
+    return $this->hasMany('Post');
+  }
 
-	/**
-	 * Get the unique identifier for the user.
-	 *
-	 * @return mixed
-	 */
-	public function getAuthIdentifier()
-	{
-		return $this->getKey();
-	}
+  /**
+   * The attributes excluded from the model's JSON form.
+   *
+   * @var array
+   */
+  protected $hidden = array('password');
 
-	/**
-	 * Get the password for the user.
-	 *
-	 * @return string
-	 */
-	public function getAuthPassword()
-	{
-		return $this->password;
-	}
+  /**
+   * Auto purge redundant attributes
+   *
+   * @var bool
+   */
+  public $autoPurgeRedundantAttributes = true;
 
-	/**
-	 * Get the e-mail address where password reminders are sent.
-	 *
-	 * @return string
-	 */
-	public function getReminderEmail()
-	{
-		return $this->email;
-	}
+  /**
+   * Get the unique identifier for the user.
+   *
+   * @return mixed
+   */
+  public function getAuthIdentifier()
+  {
+    return $this->getKey();
+  }
+
+  /**
+   * Get the password for the user.
+   *
+   * @return string
+   */
+  public function getAuthPassword()
+  {
+    return $this->password;
+  }
+
+  /**
+   * Get the e-mail address where password reminders are sent.
+   *
+   * @return string
+   */
+  public function getReminderEmail()
+  {
+    return $this->email;
+  }
 
 }
