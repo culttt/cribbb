1ca87bc1b532cf82e0e0d092a17f412736c6b8b9
Philip Brown <phil@ipbrown.com>
2013-05-11 15:21:50 +0100

    Add validation rules and Factory to Post Model

--- app/models/Post.php
+++ app/models/Post.php
@@ -10,6 +10,22 @@ class Post extends Eloquent {
   protected $fillable = array('body');
 
   /**
+   * Ardent validation rules
+   */
+  public static $rules = array(
+    'body' => 'required',
+    'user_id' => 'required'
+  );
+
+  /**
+   * Factory
+   */
+  public static $factory = array(
+    'body' => 'text',
+    'user_id' => 'factory|User',
+  );
+
+  /**
    * User relationship
    */
   public function user()
