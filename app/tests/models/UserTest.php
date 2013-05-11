24f3b2a15d3612b36661a467a4bc5a0dacc57503
Philip Brown <phil@ipbrown.com>
2013-05-11 13:50:33 +0100

    Add validation test for username in User model

--- app/tests/models/UserTest.php
+++ app/tests/models/UserTest.php
@@ -2,9 +2,28 @@
 
 class UserTest extends TestCase {
 
-  public function testThatTrueIsTrue()
+  /**
+   * Username is required
+   */
+  public function testUsernameIsRequired()
   {
-    $this->assertTrue(true);
+    // Create a new User
+    $user = new User;
+    $user->email = "phil@ipbrown.com";
+    $user->password = "password";
+    $user->password_confirmation = "password";
+
+    // User should not save
+    $this->assertFalse($user->save());
+
+    // Save the errors
+    $errors = $user->errors()->all();
+
+    // There should be 1 error
+    $this->assertCount(1, $errors);
+
+    // The username error should be set
+    $this->assertEquals($errors[0], "The username field is required.");
   }
 
 }
