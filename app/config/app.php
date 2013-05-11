6c811164d4725eb594764441d138e856adc0ff5b
Philip Brown <phil@ipbrown.com>
2013-05-11 14:59:55 +0100

    Clean up

--- app/config/app.php
+++ app/config/app.php
@@ -158,11 +158,7 @@ return array(
 		'File'            => 'Illuminate\Support\Facades\File',
 		'Form'            => 'Illuminate\Support\Facades\Form',
 		'Hash'            => 'Illuminate\Support\Facades\Hash',
-<<<<<<< HEAD
-		'Html'            => 'Illuminate\Support\Facades\Html',
-=======
 		'HTML'            => 'Illuminate\Support\Facades\Html',
->>>>>>> laravel/develop
 		'Input'           => 'Illuminate\Support\Facades\Input',
 		'Lang'            => 'Illuminate\Support\Facades\Lang',
 		'Log'             => 'Illuminate\Support\Facades\Log',
