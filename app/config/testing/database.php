76f5952aa5678bc97c7bfe9aacee5301078840a2
Philip Brown <phil@ipbrown.com>
2013-05-11 13:49:47 +0100

    Add testing database

---
+++ app/config/testing/database.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+
+  'default' => 'sqlite',
+
+  'connections' => array(
+
+    'sqlite' => array(
+      'driver'   => 'sqlite',
+      'database' => ':memory:',
+      'prefix'   => ''
+    ),
+
+  )
+  
+);
