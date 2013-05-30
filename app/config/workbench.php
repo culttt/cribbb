6fc2be05004f4664d26d71c18528c2eea863a252
Philip Brown <phil@ipbrown.com>
2013-05-30 18:43:03 +0100

    Add Workbench details

--- app/config/workbench.php
+++ app/config/workbench.php
@@ -13,7 +13,7 @@ return array(
 	|
 	*/
 
-	'name' => '',
+	'name' => 'Philip Brown',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -26,6 +26,6 @@ return array(
 	|
 	*/
 
-	'email' => '',
+	'email' => 'phil@ipbrown.com',
 
 );
