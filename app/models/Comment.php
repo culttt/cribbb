7fec81d959f4555b12635aaf4a8688adb1747a30
Philip Brown <phil@ipbrown.com>
2013-06-02 19:09:57 +0100

    Add Comments to Posts tests

--- app/models/Comment.php
+++ app/models/Comment.php
@@ -2,6 +2,30 @@
 
 class Comment extends Eloquent {
 
+  /**
+   * Properties that can be mass assigned
+   *
+   * @var array
+   */
+  protected $fillable = array('body');
+
+  /**
+   * Ardent validation rules
+   */
+  public static $rules = array(
+    'body' => 'required',
+  );
+
+  /**
+   * Factory
+   */
+  public static $factory = array(
+    'body' => 'text',
+  );
+
+  /**
+   * Comment polymorphic relationship
+   */
   public function commentable()
   {
     return $this->morphTo();
--- app/tests/unit/PostTest.php
+++ app/tests/unit/PostTest.php
@@ -88,4 +88,22 @@ class PostTest extends TestCase {
     $this->assertTrue($post->save());
   }
 
+  /**
+   *  Test adding new comment
+   */
+  public function testAddingNewComment()
+  {
+    // Create a new Post
+    $post = FactoryMuff::create('Post');
+
+    // Create a new Comment
+    $comment = new Comment(array('body' => 'A new comment.'));
+
+    // Save the Comment to the Post
+    $post->comments()->save($comment);
+
+    // This Post should have one comment
+    $this->assertCount(1, $post->comments);
+  }
+
 }
