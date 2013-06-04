a687450e17e57134e89882a53e50cbb0e7e22f89
Philip Brown <phil@ipbrown.com>
2013-06-04 17:38:16 +0100

    Add Mockery to UserControllerTest

--- app/tests/functional/UserControllerTest.php
+++ app/tests/functional/UserControllerTest.php
@@ -2,5 +2,29 @@
 
 class UserControllerTest extends TestCase {
 
+  public function setUp()
+  {
+    parent::setUp();
+
+    $this->mock = $this->mock('Cribbb\Storage\User\UserRepository');
+  }
+   
+  public function mock($class)
+  {
+    $mock = Mockery::mock($class);
+ 
+    $this->app->instance($class, $mock);
+ 
+    return $mock;
+  }
+
+  public function testIndex()
+  {
+    $this->mock->shouldReceive('all')->once();
+
+    $this->call('GET', 'user');
+
+    $this->assertResponseOk();
+  }
 
 }
