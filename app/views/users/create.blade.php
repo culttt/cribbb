0e2e0136d6793f6f95f21878b2943b3f42a6c01c
Philip Brown <phil@ipbrown.com>
2013-07-09 19:17:14 +0100

    Add users.create form

--- app/views/users/create.blade.php
+++ app/views/users/create.blade.php
@@ -1,3 +1,23 @@
-{{ Form::open(array('url' => 'users')) }}
-    
+@if($errors->any())
+  <ul>
+    {{ implode('', $errors->all('<li>:message</li>'))}}
+  </ul>
+@endif
+
+{{ Form::open(array('route' => 'users.store')) }}
+
+  <p>{{ Form::label('username', 'Username') }}
+  {{ Form::text('username') }}</p>
+
+  <p>{{ Form::label('email', 'Email') }}
+  {{ Form::text('email') }}</p>
+
+  <p>{{ Form::label('password', 'Password') }}
+  {{ Form::text('password') }}</p>
+
+  <p>{{ Form::label('password_confirmation', 'Password confirm') }}
+  {{ Form::text('password_confirmation') }}</p>
+
+  <p>{{ Form::submit('Submit') }}</p>
+
 {{ Form::close() }}
