cb6ce263f3b0181f7688f506fd746eb460fe80c2
Philip Brown <phil@ipbrown.com>
2013-07-30 19:55:16 +0100

    Add Password Controller

---
+++ app/controllers/PasswordController.php
@@ -0,0 +1,25 @@
+<?php
+
+class PasswordController extends BaseController {
+
+  public function remind()
+  {
+
+  }
+
+  public function send()
+  {
+
+  }
+
+  public function reset()
+  {
+
+  }
+
+  public function update()
+  {
+    
+  }
+
+}
