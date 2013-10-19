e300dccea8f6465929890092740433490f070bad
Philip Brown <phil@ipbrown.com>
2013-10-19 15:44:14 +0100

    Add Sass

--- GruntFile.js
+++ GruntFile.js
@@ -1,20 +1,97 @@
+/*!
+ * Cribbb Gruntfile
+ * http://cribbb.com
+ * @author Philip Brown
+ */
+
+/**
+ * Grunt Module
+ */
 module.exports = function(grunt) {
 
+  /**
+   * Configuration
+   */
   grunt.initConfig({
+
+    /**
+     * Get package meta data
+     */
     pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+
+    /**
+     * Set project object
+     */
+    project: {
+      app: 'app',
+      assets: '<%= project.app %>/assets',
+      css: [
+        '<%= project.assets %>/scss/style.scss'
+      ]
+    },
+
+    /**
+     * Project banner
+     * Dynamically appended to CSS/JS files
+     * Inherits text from package.json
+     */
+    tag: {
+      banner: '/*!\n' +
+              ' * <%= pkg.name %>\n' +
+              ' * <%= pkg.title %>\n' +
+              ' * <%= pkg.url %>\n' +
+              ' * @author <%= pkg.author %>\n' +
+              ' * @version <%= pkg.version %>\n' +
+              ' * Copyright <%= pkg.copyright %>. <%= pkg.license %> licensed.\n' +
+              ' */\n'
+    },
+
+    /**
+     * Compile Sass/SCSS files
+     * https://github.com/gruntjs/grunt-contrib-sass
+     * Compiles all Sass/SCSS files and appends project banner
+     */
+    sass: {
+      dev: {
+        options: {
+          style: 'expanded',
+          banner: '<%= tag.banner %>',
+          compass: true
+        },
+        files: {
+          '<%= project.assets %>/css/style.css': '<%= project.css %>'
+        }
       },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+      dist: {
+        options: {
+          style: 'compressed',
+          compass: true
+        },
+        files: {
+          '<%= project.assets %>/css/style.css': '<%= project.css %>'
+        }
       }
-    }
+    },
+
   });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
 
-  grunt.registerTask('default', ['uglify']);
+  /**
+   * Default task
+   * Run `grunt` on the command line
+   */
+  grunt.registerTask('default', [
+    'sass:dev'
+  ]);
+
+  /**
+   * Build task
+   * Run `grunt build` on the command line
+   * Then compress all JS/CSS files
+   */
+  grunt.registerTask('build', [
+    'sass:dist'
+  ]);
 
 };
