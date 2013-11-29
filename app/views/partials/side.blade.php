64f80d6899617b48173cd06939c7c9b63b5693d6
Philip Brown <phil@ipbrown.com>
2013-11-29 12:00:50 +0000

    Add sidebar navigation styles. Need to refactor

--- app/views/partials/side.blade.php
+++ app/views/partials/side.blade.php
@@ -5,10 +5,20 @@
   <div class="media__body user-card__username">{{{ $user->username }}}</div>
 </div>
 
-  <nav class="navigation">
+  <nav class="navigation site-navigation">
+    <ul>
+      <li><a href="#">Feed</a></li>
+      <li><a href="#">Profile</a></li>
+      <li><a href="#">Following</a></li>
+      <li><a href="#">Favourites</a></li>
+    </ul>
+  </nav>
+
+  <nav class="navigation cribbbs-list">
+    <span class="navigation__title">Your Cribbbs</span>
     <ul>
       @foreach ($cribbbs as $cribbb)
-        <li>{{{ $cribbb }}}</li>
+        <li><a href="#">{{{ $cribbb }}}</a></li>
       @endforeach
     </ul>
   </nav>
