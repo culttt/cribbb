bcaf0333c6f9fad4c7650fa80ed74f62f46c3889
Philip Brown <phil@ipbrown.com>
2014-01-04 15:57:36 +0000

    Clean up repositories

---
+++ app/Cribbb/Repository/StubRepository.php
@@ -0,0 +1,68 @@
+<?php namespace Cribbb\Repository;
+
+use Illuminate\Database\Eloquent\Model;
+
+class StubRepository implements RepositoryInterface {
+
+  /**
+   * @var Model
+   */
+  protected $model;
+
+  /**
+   * Construct
+   *
+   * @param
+   * @param
+   */
+  public function __construct(Model $model)
+  {
+    $this->model = $model;
+  }
+
+  /**
+   * All
+   *
+   * @return Illuminate\Database\Eloquent\Collection
+   */
+  public function all()
+  {
+    return $this->model->all();
+  }
+
+  /**
+   * Find
+   *
+   * @param int $id
+   * @return Illuminate\Database\Eloquent\Model
+   */
+  public function find($id)
+  {
+    return $this->model->find($id);
+  }
+
+  /**
+   * Create
+   *
+   * @param array $data
+   * @return boolean
+   */
+  public function create(array $input){}
+
+  /**
+   * Update
+   *
+   * @param array $data
+   * @return boolean
+   */
+  public function update(array $input){}
+
+  /**
+   * Delete
+   *
+   * @param int $id
+   * @return boolean
+   */
+  public function delete($id){}
+
+}
--- app/Cribbb/Repository/User/UserRepository.php
+++ app/Cribbb/Repository/User/UserRepository.php
@@ -3,45 +3,6 @@
 interface UserRepository {
 
   /**
-   * All
-   *
-   * @return object
-   */
-  public function all();
-
-  /**
-   * Find
-   *
-   * @param int $id
-   * @return object
-   */
-  public function find($id);
-
-  /**
-   * Create
-   *
-   * @param array $data
-   * @return boolean
-   */
-  public function create($data);
-
-  /**
-   * Update
-   *
-   * @param array $data
-   * @return boolean
-   */
-  public function update($data);
-
-  /**
-   * Delete
-   *
-   * @param int $id
-   * @return boolean
-   */
-  public function delete($id);
-
-  /**
    * Feed
    *
    * @param int $id
