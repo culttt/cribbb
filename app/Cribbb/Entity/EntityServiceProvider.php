65077e610c26d7fece6f9806dc80449cc96adb58
Philip Brown <phil@ipbrown.com>
2014-01-04 16:03:10 +0000

    Add User Entity

--- app/Cribbb/Entity/EntityServiceProvider.php
+++ app/Cribbb/Entity/EntityServiceProvider.php
@@ -1,5 +1,33 @@
 <?php namespace Cribbb\Entity;
 
+use Illuminate\Validation\Factory;
 use Illuminate\Support\ServiceProvider;
+use Cribbb\Entity\User\UserEntity;
+use Cribbb\Service\Validation\Laravel\User\UserCreateValidator;
+use Cribbb\Service\Validation\Laravel\User\UserUpdateValidator;
 
-class EntityServiceProvider extends ServiceProvider {}
+class EntityServiceProvider extends ServiceProvider {
+
+  /**
+   * Register the binding
+   *
+   * @return void
+   */
+  public function register()
+  {
+    /**
+     * User Entity
+     *
+     * @return Cribbb\Entity\User\UserEntity
+     */
+    $this->app->bind('Cribbb\Entity\User\UserEntity', function($app)
+    {
+      return new UserEntity(
+        $app->make('Cribbb\Repository\User\UserRepository'),
+        new UserCreateValidator( $app['validator'] ),
+        new UserUpdateValidator( $app['validator'] )
+      );
+    });
+  }
+
+}
---
+++ app/Cribbb/Entity/User/UserEntity.php
@@ -0,0 +1,45 @@
+<?php namespace Cribbb\Entity\User;
+
+use Cribbb\Entity\AbstractEntity;
+use Cribbb\Entity\EntityInterface;
+use Cribbb\Repository\User\UserRepository;
+use Cribbb\Service\Validation\Laravel\User\UserCreateValidator;
+use Cribbb\Service\Validation\Laravel\User\UserUpdateValidator;
+
+class UserEntity extends AbstractEntity implements EntityInterface {
+
+  /**
+   * @var Cribbb\Repository\User\UserRepository
+   */
+  protected $repository;
+
+  /**
+   * @var Cribbb\Service\Validation\Laravel\UserCreateValidator
+   */
+  protected $createValidator;
+
+  /**
+   * @var Cribbb\Service\Validation\Laravel\UserUpdateValidator
+   */
+  protected $updateValidator;
+
+  /**
+   * @var Illuminate\Support\MessageBag
+   */
+  protected $errors;
+
+  /**
+   * Construct
+   *
+   * @param Cribbb\Repository\User\UserRepository $repository
+   * @param Cribbb\Service\Validation\Laravel\UserCreateValidator $createValidator
+   * @param Cribbb\Service\Validation\Laravel\UserUpdateValidator $updateValidator
+   */
+  public function __construct(UserRepository $repository, UserCreateValidator $createValidator, UserUpdateValidator $updateValidator)
+  {
+    $this->repository = $repository;
+    $this->createValidator = $createValidator;
+    $this->updateValidator = $updateValidator;
+  }
+
+}
