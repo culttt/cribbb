26393b8ea27521d06ac550c31cc73714e90f6624
philipbrown <phil@ipbrown.com>
2014-02-16 16:36:36 +0000

    Move the Cache service out of the Services directory

---
+++ app/Cribbb/Cache/CacheInterface.php
@@ -0,0 +1,31 @@
+<?php namespace Cribbb\Cache;
+
+interface CacheInterface {
+
+  /**
+   * Get
+   *
+   * @param string $key
+   * @return mixed
+   */
+  public function get($key);
+
+  /**
+   * Put
+   *
+   * @param string $key
+   * @param mixed $value
+   * @param integer $minutes
+   * @return mixed
+   */
+  public function put($key, $value, $minutes = null);
+
+  /**
+   * Has
+   *
+   * @param string $key
+   * @return bool
+   */
+  public function has($key);
+
+}
---
+++ app/Cribbb/Cache/LaravelCache.php
@@ -0,0 +1,76 @@
+<?php namespace Cribbb\Cache;
+
+use Illuminate\Cache\CacheManager;
+
+class LaravelCache implements CacheInterface {
+
+ /**
+  * @var Illuminate\Cache\CacheManager
+  */
+  protected $cache;
+
+  /**
+   * @var string
+   */
+  protected $tag;
+
+  /**
+   * @var integer
+   */
+  protected $minutes;
+
+  /**
+   * Construct
+   *
+   * @param Illuminate\Cache\CacheManager $cache
+   * @param string $tag
+   * @param integer $minutes
+   */
+  public function __construct(CacheManager $cache, $tag, $minutes = 60)
+  {
+    $this->cache = $cache;
+    $this->tag = $tag;
+    $this->minutes = $minutes;
+  }
+
+  /**
+   * Get
+   *
+   * @param string $key
+   * @return mixed
+   */
+  public function get($key)
+  {
+    return $this->cache->tags($this->tag)->get($key);
+  }
+
+  /**
+   * Put
+   *
+   * @param string $key
+   * @param mixed $value
+   * @param integer $minutes
+   * @return mixed
+   */
+  public function put($key, $value, $minutes = null)
+  {
+    if( is_null($minutes) )
+    {
+      $minutes = $this->minutes;
+    }
+
+    return $this->cache->tags($this->tag)->put($key, $value, $minutes);
+  }
+
+  /**
+   * Has
+   *
+   * @param string $key
+   * @return bool
+   */
+  public function has($key)
+  {
+    return $this->cache->tags($this->tag)->has($key);
+  }
+
+}
