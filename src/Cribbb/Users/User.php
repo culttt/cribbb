24a9dd6fdbd29a170bda287d6c6b43e02c1e1d42
philipbrown <phil@ipbrown.com>
2014-07-20 13:03:54 +0100

    Add Cribbb Many-to-Many relationship to the User entity

--- src/Cribbb/Users/User.php
+++ src/Cribbb/Users/User.php
@@ -1,9 +1,11 @@
 <?php namespace Cribbb\Users;
 
-use Doctrine\ORM\Mapping as ORM;
+use Cribbb\Cribbbs\Cribbb;
 use Cribbb\Users\Email\Email;
+use Doctrine\ORM\Mapping as ORM;
 use Cribbb\Users\Username\Username;
 use Cribbb\Users\Password\Password;
+use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * @ORM\Entity
@@ -33,43 +35,107 @@ class User {
    */
   private $password;
 
+  /**
+   * @ORM\ManyToMany(targetEntity="Cribbb\Cribbbs\Cribbb", inversedBy="users")
+   */
+  private $cribbbs;
+
+  /**
+   * Create a new User instance
+   *
+   * @param Cribbb\Users\Email\Email $email
+   * @param Cribbb\Users\Username\Username $username
+   * @param Cribbb\Users\Password\Password $password
+   * @return void
+   */
   public function __construct(Email $email, Username $username, Password $password)
   {
     $this->setEmail($email);
     $this->setUsername($username);
     $this->setPassword($password);
+
+    $this->cribbbs = new ArrayCollection();
   }
 
+  /**
+   * Add the user to a Cribbb
+   *
+   * @param Cribbb\Cribbbs\Cribbb $cribbb
+   * @return void
+   */
+  public function addToCribbb(Cribbb $cribbb)
+  {
+    $this->cribbbs[] = $cribbb;
+  }
+
+  /**
+   * Get the User's id
+   *
+   * @return int
+   */
   public function getId()
   {
     return $this->id;
   }
 
+  /**
+   * Get the User's email address
+   *
+   * @return string
+   */
   public function getEmail()
   {
     return $this->email;
   }
 
+  /**
+   * Set the User's email address
+   *
+   * @param Cribbb\Users\Email\Email $email
+   * @return void
+   */
   public function setEmail(Email $email)
   {
     $this->email = $email;
   }
 
+  /**
+   * Get the User's username
+   *
+   * @return string
+   */
   public function getUsername()
   {
     return $this->username;
   }
 
+  /**
+   * Set the User's username
+   *
+   * @param Cribbb\Users\Username\Username
+   * @return void
+   */
   public function setUsername(Username $username)
   {
     $this->username = $username;
   }
 
+  /**
+   * Get the User's password
+   *
+   * @return string
+   */
   public function getPassword()
   {
     return $this->username;
   }
 
+  /**
+   * Set the User's password
+   *
+   * @param Cribbb\Users\Password\Password
+   * @return void
+   */
   public function setPassword(Password $password)
   {
     $this->password = $password;
