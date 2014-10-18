86c6413ee8f871ef3d32f75c22a5767f20491e10
philipbrown <phil@ipbrown.com>
2014-10-18 10:08:32 +0100

    Finish off CommandBus implementation

--- src/Application/CommandBus.php
+++ src/Application/CommandBus.php
@@ -8,14 +8,20 @@ class CommandBus
     private $container;
 
     /**
+     * @var Inflector
+     */
+    private $inflector;
+
+    /**
      * Create a new CommandBus
      *
      * @param Container $container
      * @return void
      */
-    public function __construct(Container $container)
+    public function __construct(Container $container, Inflector $inflector)
     {
         $this->container = $container;
+        $this->inflector = $inflector;
     }
 
     /**
@@ -24,5 +30,20 @@ class CommandBus
      * @param Command $command
      * @return void
      */
-    public function execute(Command $command){}
+    public function execute(Command $command)
+    {
+        $this->handler($command)->handle($command);
+    }
+
+    /**
+     * Get the Command Handler
+     *
+     * @return mixed
+     */
+    private function handler(Command $command)
+    {
+        $class = $this->inflector->inflect($command);
+
+        return $this->container->make($class);
+    }
 }
