c49a667f84dd82192f4017692881b3ffbb9c115b
Philip Brown <phil@ipbrown.com>
2015-03-01 15:52:49 +0000

    Add ember.js

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,12 +1,6 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+Route::get('{data?}', function()
+{
+	return View::make('app');
+})->where('data', '.*');
